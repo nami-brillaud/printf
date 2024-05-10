b6ed493c93f6269032646fd7e6d79af9cb7bc398
Nami Fujisaki <nfujisak@c2r6s2.42tokyo.jp>
2024-05-10 21:04:30 +0900

    norminette ok

--- ft_diux.c
+++ ft_diux.c
@@ -6,13 +6,13 @@
 /*   By: nfujisak <nfujisak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 16:20:46 by nfujisak          #+#    #+#             */
-/*   Updated: 2024/05/10 18:44:35 by nfujisak         ###   ########.fr       */
+/*   Updated: 2024/05/10 21:04:19 by nfujisak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int d_i_process(int nb)
+int	d_i_process(int nb)
 {	
 	char	*str;
 	int		digits;
@@ -29,11 +29,11 @@ int	u_process(unsigned int nb)
 	unsigned long	ul;
 	int				digits;
 
-	ul = (unsigned long)nb;//this wraps negative values automatically !!
+	ul = (unsigned long)nb;
 	digits = 1;
-	if (nb >= 10) //probably not nb
-		digits += u_process(nb / 10); //counting the digits 100 -> 10 (2))
-	ft_putchar_fd((ul % 10) + '0', 1); //print rightmost digit one by one (as printing doesn't care about size.)
+	if (nb >= 10)
+		digits += u_process(nb / 10);
+	ft_putchar_fd((ul % 10) + '0', 1);
 	return (digits);
 }
 
@@ -42,7 +42,7 @@ int	x_process(unsigned int nb)
 	char	*str;
 	int		len;
 
-	str = ft_tolower_all(base_start((unsigned long long)nb, 16)); //???? the cast
+	str = ft_tolower_all(base_start((unsigned long long)nb, 16));
 	len = ft_strlen(str);
 	ft_putstr_fd(str, 1);
 	free(str);
@@ -54,11 +54,9 @@ int	bigx_process(unsigned int nb)
 	char	*str;
 	int		len;
 
-	str = ft_toupper_all(base_start((unsigned long long)nb, 16)); //???? the cast
+	str = ft_toupper_all(base_start((unsigned long long)nb, 16));
 	len = ft_strlen(str);
 	ft_putstr_fd(str, 1);
 	free(str);
 	return (len);
 }
-
-
